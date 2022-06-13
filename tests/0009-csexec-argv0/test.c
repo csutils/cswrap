dfdff6d671a14102c22b8006b1ef1c1c07895d50
Lukáš Zaoral <lzaoral@redhat.com>
2022-06-13 14:19:20 +0200

    tests: fully enable csexec-argv0 test on glibc 2.34+
    The issue with auxiliary vector was fixed in glibc 2.35 and was backported
    to glibc 2.34.

--- tests/0009-csexec-argv0/test.c
+++ tests/0009-csexec-argv0/test.c
@@ -5,10 +5,11 @@
 
 int main(int argc, char **argv)
 {
-    /* ld.so ./executable corrupts auxiliary vector on armv7 and aarch64
-       See https://sourceware.org/bugzilla/show_bug.cgi?id=23293
+    /* ld.so version 2.33 or older corrupts auxiliary vector on armv7
+     * or aarch64.  See https://sourceware.org/bugzilla/show_bug.cgi?id=23293
      */
-#if !__arm__ && !__aarch64__
+#if (!__arm__ && !__aarch64__) || __GLIBC__ > 2 || \
+        (__GLIBC__ == 2 && __GLIBC_MINOR__ > 33)
     const char *at_execfn = (const char *) getauxval(AT_EXECFN);
     assert(strcmp(at_execfn, argv[0]) != 0);
 #endif
